6574ec9e96fd165d499532d01201426382bd1d0b
Lena <lena.shlyakhovets@gmail.com>
2023-04-29 00:32:11 +0300

    2

--- cypress/e2e/testDemoQa.cy.js
+++ cypress/e2e/testDemoQa.cy.js
@@ -3,13 +3,18 @@
 
 describe('first test', () => {
     beforeEach(() => {
-        cy.visit('https://demoqa.com/')
+        cy.visit('https://openweathermap.org/')
     })
 
-
+    it.skip('verify link1', () => {
+        cy.get('div.category-cards .card-body h5').contains('Elements').click()
+        cy.get('#item-0 span').contains('Text Box').click()
+        cy.get('#userName').type('Maria')
+    })
+    
     it('verify link1', () => {
         cy.get('div.category-cards .card-body h5').contains('Elements').click()
         cy.get('#item-0 span').contains('Text Box').click()
         cy.get('#userName').type('Maria')
-    })  
+    })
 })
